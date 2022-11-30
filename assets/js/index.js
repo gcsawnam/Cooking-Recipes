af6a3258626fef2be952283b30e1d732ea07aa0e
gcsawnam <gcsawnam@gmail.com>
2022-11-30 17:00:50 +0545

    some changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
+function menuWrapper() {
+    var x = document.getElementById("primary-menu");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   } 
+  
+  function searchIcon(){
+   document.getElementById("search").style.visibility = "visible";;
+    
+     
+  }
