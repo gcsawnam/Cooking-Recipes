ab32b5ac04d44198be58b308a4e30bb4b12e68c5
gcsawnam <gcsawnam@gmail.com>
2022-12-12 16:58:21 +0545

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,63 +1,31 @@
-function menuWrapper() {
-  console.log('hello world');
-  var x = document.getElementById("primary-menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function menuWrapper() {
+//   console.log('hello world');
+//   var x = document.getElementById("primary-menu");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
 
 function closeIcons(){
-  
-  
   var y = document.getElementById('primary-id-items1');
   var z = document.getElementById('primary-id-items2');
   var w = document.getElementById('celsius-header-search-icons');
   var A = document.getElementById('celsius-header-close-icons');
-{
-  {
-    y.style.display = "block";
-  }
-  {
-    z.style.display = "none";
-  }
-  {
-    w.style.display = 'block';
-  }
-
-  {
-  A.style.display = 'none';
-  console.log('hwlo');
-  }
-
-}
-
-}
+  y.style.display = "block";
+  z.style.display = "none";
+   w.style.display = 'block';
+   A.style.display = 'none';
+   }
 function searchIcon() {
   console.log('hello world');
   var y = document.getElementById('primary-id-items1');
   var z = document.getElementById('primary-id-items2');
   var w = document.getElementById('celsius-header-search-icons');
   var A = document.getElementById('celsius-header-close-icons');
-  console.log('hy');
-  
-  {
     y.style.display = "none";
-  }
-  {
     z.style.display = "block";
-  }
-
-  {
     w.style.display = 'none';
-  }
-
-  {
   A.style.display = 'block';
-  console.log('hwlo');
-  }
-
-
-
 }
