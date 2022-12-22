6af25ae678d7c0428352bcc089c6d3a78519dc25
gcsawnam <gcsawnam@gmail.com>
2022-12-22 17:17:21 +0545

    slick -slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,37 @@
 $(document).ready(function () {
   $(".celsius-header-search-icons").click(function () {
     $(this).css("display", "none");
-    $("#primary-id-items1").css("display", "none");
+    $('body').fadeTo('slow', 0.5);
+   $("#primary-id-items1").css("display", "none");
     $("#primary-id-items2").css("display", "block");
     $("#celsius-header-close-icons").css("display", "block");
   }),
     $("#celsius-header-close-icons").click(function () {
       $(this).css("display", "none");
-      $("#primary-id-items1").css("display", "block");
+    $('body').fadeTo('slow', 1);
+
+    $("#primary-id-items1").css("display", "block");
       $("#primary-id-items2").css("display", "none");
       $(".celsius-header-search-icons").css("display", "block");
     });
 });
+
 
 $(document).ready(function () {
   $(".celsius-search-item-wrap-main-section").slick({
     infinite: false,
     speed: 300,
     arrows: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: false,
+    // adaptiveHeight: true,
+    slidesToShow: 3,
+     slidesToScroll: 1,
+     initialSlide: 0,
+     draggable: false,
+     adaptiveHeight: true,
+     
+     
+    // centerMode: true,
+    // centerPadding: '20%',
   });
 
   $(".celsius-search-second-item-wrap-main-section").slick({
