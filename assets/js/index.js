17294bfd8fd4f44189b52dffdc28cac5c8127df8
gcsawnam <gcsawnam@gmail.com>
2023-01-02 17:23:06 +0545

    scss changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,81 @@
 $(document).ready(function () {
-  $(".celsius-header-search-icons").click(function () {
-    $(this).css("display", "none");
-    $("body").fadeTo("slow", 0.5);
+//  document.getElementsByClassName('celsius-header-search-icons');
+ $('.celsius-header-heart-icon').click(function(e){
+ e.preventDefault();
+  var test=$('.celsius-header-heart-icon').parentElement;
+console.log(test);
+ })
+
+
+//   $("body").click(function(e) {
+//     if ($(e.target).attr('id') == 'main-head') {
+//         return;
+//     } else {
+//         $('#main-head').fadeTo("slow", 0.5);
+//     }
+// });
+
+
+
+  $(".fa-search").click(function (e) {
+  e.preventDefault();
+
+    $(".celsius-navbar-searchIcons").css("display", "block");
+    $(".celsius-header-search-icons .fa-search").css("display","none");
+    // $("body").fadeTo("slow", 0.5);
+    // $("body").addClass("celsius-whole-overlay");
     $("#primary-id-items1").css("display", "none");
+    $(".celsius-header-heart-icon,.celsius-dropdown").css("visibility", "hidden");
+
     $("#primary-id-items2").css("display", "block");
-    $("#celsius-header-close-icons").css("display", "block");
+    $(".close-icons").css("display", "block");
    $('.celsius-overlay-menu-bar,.celsius-cooking-site-branding,.celsius-dropdown ,.celsius-header-heart-icon ').addClass('menu-item-display');
    $('.celsius-header-right-items ').addClass('menu-item-display1');
      
 
   }),
-    $("#celsius-header-close-icons").click(function () {
-      $(this).css("display", "none");
-      $("body").fadeTo("slow", 1);
+    $(".fa-xmark").click(function (e) {
+  e.preventDefault();
+
+      $(".close-icons").css("display", "none");
+      // $("body").fadeTo("slow", 1);
+    // $("body").removeClass("celsius-whole-overlay");
+    $(".celsius-header-search-icons .fa-search").css("display","block");
+  
       $("#primary-id-items1").css("display", "block");
+      $(".celsius-header-heart-icon,.celsius-dropdown").css("visibility","visible");
+
       $("#primary-id-items2").css("display", "none");
       $(".celsius-header-search-icons").css("display", "block");
    $('.celsius-overlay-menu-bar,.celsius-cooking-site-branding,.celsius-dropdown ,.celsius-header-heart-icon ').removeClass('menu-item-display');
    $('.celsius-header-right-items ').removeClass('menu-item-display1');
-       
+    
     }),
+      $('body').click(function()
+      {})
+
+
+
+
+    $("#celsius-overlay1").click(function () {
+      $("body").removeClass("celsius-whole-overlay");
+ 
+
+    }),
+
+
+
     $(".celsius-overlay-menu-bar").click(function () {
       $(".menuitems , #primary-id-items1").css("left", "0vw");
-
-       }),
-
-
+  }),
        $(".fa-angle-down").click(function () {
-
-        console.log('button is clicked ')
-        $("#recipes-items").toggle();
-      
-       
+         $("#recipes-items").toggle(); 
        }),
-       
-
        $(".fa-xmark").click(function () {
+
       $(".menuitems , #primary-id-items1").css("left", "-100vw");
-
-         }),
-     
-
- $(".celsius-search-item-wrap-main-section").slick({
+  }),
+ $(".celsius-search-item-wrap-main-section,.celsius-search-second-item-wrap-main-section").slick({
     infinite: false,
     speed: 300,
     arrows: true,
@@ -50,15 +84,7 @@ $(document).ready(function () {
     initialSlide: 0,
     draggable: false,
     adaptiveHeight: true,
-  }),
-  $(".celsius-search-second-item-wrap-main-section").slick({
-    infinite: false,
-    speed: 300,
-    arrows: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: false,
-  });
+  })
 });
 
 
